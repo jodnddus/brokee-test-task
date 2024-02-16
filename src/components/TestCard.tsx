10f62f5192703cf69e87e9c2010e7c0e0bae6108
jodnddus <lockhost23@gmail.com>
2024-02-16 17:49:48 +0900

    test card design

--- src/components/TestCard.tsx
+++ src/components/TestCard.tsx
@@ -2,13 +2,31 @@ import React from "react";
 
 import { TestType } from "../type/test";
 
-function TestCard({ category, name, featured_image: testThumbnail }: TestType) {
+function TestCard({ category, name, featured_image: testThumbnail, description_short, difficulty }: TestType) {
+    const splitName = (name: string) => {
+        if (name.includes(":")) {
+            return name.split(":");
+        }
+
+        return [name];
+    }
+
     return (
         <div className="test-card">
             <div className="test-thumbnail-container">
                 <img className="test-thumbnail" src={testThumbnail} alt={`${category} image`} />
             </div>
-            {name}
+            <div className="test-info-container">
+                <div className="test-name-box">
+                    {splitName(name).map((item, index) => <span className={`test-name ${index == 0 ? "category" : "name"}`}>{item}</span>)}
+                </div>
+
+                <p className="test-description">{description_short}</p>
+                <div className="test-infomation-box">
+                    <span className="test-difficulty-name">DIFFICULTY</span>
+                    <span className={`test-difficulty ${difficulty.toUpperCase()}`}>{difficulty.toUpperCase()}</span>
+                </div>
+            </div>
         </div>
     )
 }
