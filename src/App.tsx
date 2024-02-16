7c00290d9e6149e195983593cc560d8e3e099466
jodnddus <lockhost23@gmail.com>
2024-02-16 16:17:00 +0900

    test call graphql api (using fetch)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+    useEffect(() => {
+        fetch('https://graph.brokee.io/v1/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                operationName: "GetChallenges",
+                query: `
+                    query GetChallenges @cached(ttl: 300, refresh: true) {
+                        tests(order_by: {name: asc}) {
+                            name
+                            brokee_id
+                            category
+                            description_short
+                            difficulty
+                            featured_image
+                            roles
+                            type
+                            status
+                            tech_stack
+                            requires_payment
+                            __typename
+                        }
+                    }
+                `,
+            }),
+        })
+            .then(response => response.json())
+            .then(data => console.log(data.data.tests))
+    },[]);
+
     return (
         <div className="App">
             Hello
